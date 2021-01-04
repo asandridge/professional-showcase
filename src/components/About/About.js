7fb50fc139529dc15b5cdc666bd435f3e820d135
Alex Sandridge <asandridge@spotx.tv>
2021-01-04 13:35:59 -0700

    Add info to the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,25 @@
 import appStyles from "../../App.module.css";
+import { Fab } from "@material-ui/core";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const About = () => {
     return (
         <div className={appStyles.contentBlock}>
+
             <h1>Alex Sandridge</h1>
-            Some info about me.
+            <div>1052 Sleepy Hollow Road, Golden Colorado, 80401</div>
+            <div>303-330-1197</div>
+            <div>alex.sandridge@gmail.com</div>
+
+            <div style={{marginTop: '5%'}}>Some info about me.</div>
+
+            <div style={{display: 'flex', width: '100%', marginTop: '5%'}}>
+                <Fab color="primary" href="https://github.com/asandridge" style={{marginRight: '5%'}}><GitHubIcon /></Fab>
+                <Fab color="primary" href="https://www.linkedin.com/in/alexander-sandridge-7117a9175/" style={{marginRight: '5%'}}><LinkedInIcon /></Fab>
+                <Fab color="primary" href="https://www.facebook.com/alex.sandridge/" style={{marginRight: '5%'}}><FacebookIcon /></Fab>
+            </div>
         </div>
     )
 }
