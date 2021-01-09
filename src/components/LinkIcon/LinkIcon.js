01058617913621add8820253baa44137615e5f7f
Alex Sandridge <asandridge@spotx.tv>
2021-01-09 06:00:04 -0700

    Add alt to link icon

--- src/components/LinkIcon/LinkIcon.js
+++ src/components/LinkIcon/LinkIcon.js
@@ -2,7 +2,7 @@ import tabIcon from "../../images/new-tab.png";
 
 const LinkIcon = () => {
     return (
-        <img src={tabIcon} style={{height: '2.5vh', paddingBottom: '1vh'}} />
+        <img src={tabIcon} style={{height: '2.5vh', paddingBottom: '1vh'}} alt="New Tab"/>
     )
 }
 
