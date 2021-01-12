fe5337c3a21a9f2496db69ff2a98c8cab64e0650
Alex Sandridge <asandridge@spotx.tv>
2021-01-11 20:40:11 -0700

    Fix broken youtube links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -24,7 +24,7 @@ const Portfolio = () => {
             <br />
             <Project 
                 title="Senior Capstone Project"
-                demoLink="youtu.be/qjjxnmxexb0"
+                demoLink="https://www.youtube.com/watch?v=qjJxNMXExb0"
                 sourceLink="https://github.com/asandridge/psych-capstone"
                 description="A mobile application developed by my team for the senior capstone class of 2020. Sponsored by the CU Boulder
                 Psychology Department, who intends to use the app to study Cross-Racial Recognition Deficit, a phenomenon where
@@ -35,7 +35,7 @@ const Portfolio = () => {
             <br />
             <Project 
                 title="Big Data Architecture Project"
-                demoLink="youtu.be/zants1ozq6i"
+                demoLink="https://www.youtube.com/watch?v=zants1OzQ6I"
                 sourceLink="https://github.com/asandridge/road-trip-app"
                 description="A single-page web application designed to help users plan custom road trips using the Google Maps API. Uses a
                 variety of technologies including Angular, Express.js, MongoDB, Kubernetes, and Docker. Built as a team project
